7e2cea8f2c92f8fc082f872d9256999c82f98450
agung <agunghandoko112@gmail.com>
2022-03-08 14:56:01 +0700

    add packages

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/index.css";
 import "react-ig-feed/dist/index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../styles/style.css"
+import "../styles/style.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
